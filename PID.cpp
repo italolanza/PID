d3438b06101cc5d5914c94a149c178e69a49155f
Italo Lanza <itmilhomem@gmail.com>
2017-05-18 17:17:12 -0300

    Add files via upload

---
+++ PID.cpp
@@ -0,0 +1,93 @@
+#include <PID.h>
+#include <Arduino.h>
+
+PID::PID (double _kP, double _kI, double _kD)
+{
+  this->setKP(_kP);
+  this->setKI(_kI);
+  this->setKD(_kD);
+	P			      =	0;
+	I			      =	0;
+	D			      =	0;
+	lastError	  =	0;
+  lastProcess = 0;
+}
+//construtor com a opção de adicionar o setPoint logo na criação do objeto
+PID::PID (double _kP, double _kI, double _kD, double _setPoint)
+{
+  this->setKP(_kP);
+  this->setKI(_kI);
+  this->setKD(_kD);
+  this->setSetPoint(_setPoint);
+	P			      =	0;
+	I			      =	0;
+	D			      =	0;
+	lastError	  =	0;
+  lastProcess = 0;
+}
+
+void PID::addNewSample (double _sample)
+{
+    sample = _sample;
+}
+
+void PID::setSetPoint( double _setPoint)
+{
+    setPoint = _setPoint;
+}
+
+void PID::setKP(double _kP)
+{
+    kP = _kP;
+}
+
+void PID::setKI(double _kI)
+{
+    kI = _kI;
+}
+
+void PID::setKD(double _kD)
+{
+    kD = _kD;
+}
+
+double PID::getKP()
+{
+    return kP;
+}
+
+double PID::getKI()
+{
+    return kI;
+}
+
+void PID::getKD()
+{
+    return kD;
+}
+
+double PID::process ()
+{
+    // Implementação PID
+        
+		error = setPoint - sample;    
+		float deltaTime = (millis() - lastProcess) / 1000.0; //deltaTime esta em segundos
+		lastProcess = millis();
+	
+	
+		//P - erro proporcional
+		P = error * kP;
+		
+		//I - "somatório dos erros"
+		I = I + (error * kI) * deltaTime;
+		
+		//D - "derivara do erro"
+		D = (lastError - error) * kD;
+		lastError = error;
+		
+		// Soma todas as contribuições
+		pid = P + I + D;
+		
+    return pid;
+}
+
---
+++ PID.h
@@ -0,0 +1,37 @@
+/*
+    An adptation of the Simple PID Class of Ivan Seidel
+    Link: https://gist.github.com/ivanseidel/b1693a3be7bb38ff3b63
+
+    Free to use 
+*/
+#ifndef PID_H
+#define PID_H
+
+class PID
+{
+    public:
+            PID(double,double,double);
+    	    PID(double,double,double,double);
+            void addNewSample (double);
+            void setSetPoint(double);
+            void setKP(double);
+            void setKI(double);
+            void setKD(double);
+	    double getKP();
+	    double getKI();
+	    double getKD();
+	    double process();
+
+    private:
+            double error;
+            double lastError;
+            double sample;
+            double kP, kI, kD;
+            double P, I, D;
+            double pid;
+            double setPoint;
+	    long lastProcess;
+           
+
+};
+#endif
